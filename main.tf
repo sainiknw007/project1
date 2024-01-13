e49235a87fcb4404fc49f8388427472db8c9d3c0
sainiknw007 <sainiknw007@gmail.com>
2024-01-13 23:42:47 +0530

    updated

--- main.tf
+++ main.tf
@@ -53,10 +53,11 @@ resource "aws_instance" "myFirstInstance" {
 }
 
 # Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
+/*resource "aws_eip" "myFirstInstance" {
   vpc      = true
   instance = aws_instance.myFirstInstance.id
 tags= {
     Name = "my_elastic_ip"
   }
 }
+*/
