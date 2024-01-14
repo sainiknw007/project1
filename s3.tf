2ee1728a046add8b14161d244c314914039c5ba7
sainiknw007 <sainiknw007@gmail.com>
2024-01-14 08:11:41 +0530

    added versioning code

--- s3.tf
+++ s3.tf
@@ -2,10 +2,12 @@ resource "aws_s3_bucket" "my-s3-bucket1" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
   tags = var.tags
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my-s3-bucket1.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
