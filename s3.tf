d79158df0bff4c3b472dbbe175de177e2a20fd88
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2024-10-13 18:29:42 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,17 +1,14 @@
-resource "aws_s3_bucket" "my-s3-bucket1" {
-  bucket_prefix = var.bucket_prefix
-  
-  tags = var.tags
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
 }
 
-resource "aws_s3_bucket_acl" "my-s3-bucket_acl" {
-  bucket = aws_s3_bucket.my-s3-bucket1.id
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
 
-
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.my-s3-bucket1.id
+  bucket = aws_s3_bucket.example.id
   versioning_configuration {
     status = "Enabled"
   }
