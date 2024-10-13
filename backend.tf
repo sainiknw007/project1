b3cf58a48c261bfef8af718ea4a16f45c6748efe
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2024-10-13 10:05:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraform-batch-01"
-    key = "main"
+    key = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
