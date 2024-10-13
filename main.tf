1a4910803b6a44502cc3bcbdda8d62c18ad1caf9
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2024-10-13 16:52:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "<5.71.0"  # Use a version prior to 5.71.0
     }
   }
 }
+
 
 provider "aws" {
   region = var.aws_region
