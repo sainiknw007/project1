a27d9e0c8abb3888ffb7f339cbc4f2e265af97b1
Akhilesh Saini <146843314+sainiknw007@users.noreply.github.com>
2024-10-13 09:59:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "okay" 
+    default     =  "main" 
 }
 
 variable "instance_type" { 
